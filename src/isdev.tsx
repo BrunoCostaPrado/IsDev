f591cedd10a4d90f5d1f69d5583014ec80eaff81
Bruno Costa Prado <brunocostaprado@gmail.com>
2025-04-03 23:57:01 -0300

    remove console.log

--- src/isdev.tsx
+++ src/isdev.tsx
@@ -2,7 +2,6 @@ export function DevOnly({
   children,
 }: { children: React.ReactNode; isDev: unknown }) {
   if (process.env.NODE_ENV === "development") {
-    console.log("Dev prop is being used!")
     return <>{children}</>
   }
 }
